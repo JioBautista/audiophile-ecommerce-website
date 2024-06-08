219889d03110e54f7919521aafc1919db783069e
Jio Bautista <98562544+JioBautista@users.noreply.github.com>
2024-06-08 18:45:07 -0400

    Updates

--- frontend/app/src/pages/checkout/Checkout.jsx
+++ frontend/app/src/pages/checkout/Checkout.jsx
@@ -1,21 +1,25 @@
 import React from "react";
 import CheckoutDetails from "./CheckoutDetails";
 import Summary from "./Summary";
+import CheckoutModal from "./CheckoutModal";
 import { useForm } from "react-hook-form";
 
 function Checkout() {
   const { register, handleSubmit } = useForm();
   const onSubmit = (data) => console.log(data);
   return (
-    <div className="bg-gray-100 p-5">
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className="container mx-auto space-y-5 lg:flex lg:items-start lg:gap-5 lg:space-y-0 lg:p-10"
-      >
-        <CheckoutDetails register={register} />
-        <Summary />
-      </form>
-    </div>
+    <>
+      <div className="bg-gray-100 p-5">
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="container mx-auto space-y-5 lg:flex lg:items-start lg:gap-5 lg:space-y-0 lg:p-10"
+        >
+          <CheckoutDetails register={register} />
+          <Summary />
+        </form>
+      </div>
+      <CheckoutModal />
+    </>
   );
 }
 
---
+++ frontend/app/src/pages/checkout/CheckoutModal.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function CheckoutModal() {
+  return (
+    <>
+      <div>Modal</div>
+    </>
+  );
+}
+
+export default CheckoutModal;
