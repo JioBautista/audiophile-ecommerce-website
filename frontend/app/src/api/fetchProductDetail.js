2f79ca631bf8c742207d61482dae9db5f24c8b5e
Jio Bautista <98562544+JioBautista@users.noreply.github.com>
2024-06-09 12:03:19 -0400

    Updates

--- frontend/app/src/api/fetchProductDetail.js
+++
@@ -1,9 +0,0 @@
-export async function fetchProductDetail(productId) {
-  try {
-    const response = await fetch("/src/data/data.json");
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.log(error);
-  }
-}
--- frontend/app/src/api/fetchProducts.js
+++
@@ -1,8 +0,0 @@
-export async function fetchProducts() {
-  try {
-    const response = await fetch("/src/data/data.js");
-    return response.json();
-  } catch (error) {
-    console.log(error);
-  }
-}
