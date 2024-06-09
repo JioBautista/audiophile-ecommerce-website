59d7dc5b941c60d4df52e03dec57c94c1d860f08
Jio Bautista <98562544+JioBautista@users.noreply.github.com>
2024-06-09 11:50:34 -0400

    Updated data

--- frontend/app/src/pages/categories/Speakers.jsx
+++ frontend/app/src/pages/categories/Speakers.jsx
@@ -2,22 +2,9 @@ import React from "react";
 import Products from "./Products";
 import Category from "../../navbar/Category";
 import EndContent from "../../footer/EndContent";
-import { useQuery } from "@tanstack/react-query";
-import { fetchProducts } from "../../api/fetchProducts";
+import { data } from "../../data/data";
 
 function Speakers() {
-  const { isPending, isError, data } = useQuery({
-    queryKey: ["headphones"],
-    queryFn: fetchProducts,
-  });
-
-  if (isPending) {
-    return <h1>Loading ...</h1>;
-  }
-  if (isError) {
-    return <h1>Error occured...</h1>;
-  }
-
   const speakers = data && data.filter((obj) => obj.category === "speakers");
 
   return (
