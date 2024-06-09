075e33e5a6f8e69ff427c420bca37e696270994d
Jio Bautista <98562544+JioBautista@users.noreply.github.com>
2024-06-08 20:27:41 -0400

    Update PaymentDetails.jsx

--- frontend/app/src/pages/checkout/PaymentDetails.jsx
+++ frontend/app/src/pages/checkout/PaymentDetails.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 function PaymentDetails({ register }) {
+  const [cash, setCash] = React.useState(false);
   const inputStyles = {
     className:
       "border-gray-300 border-2 w-full px-5 py-4 rounded-md focus:outline-orange-500  focus:invalid:outline-red-500 cursor-pointer",
@@ -33,7 +34,10 @@ function PaymentDetails({ register }) {
             </label>
           </div>
 
-          <div className="w-full cursor-pointer rounded-md border-2 border-gray-300 px-5 py-4 focus-within:border-orange-500  focus:outline-orange-500 focus:invalid:outline-red-500">
+          <div
+            className="w-full cursor-pointer rounded-md border-2 border-gray-300 px-5 py-4 focus-within:border-orange-500  focus:outline-orange-500 focus:invalid:outline-red-500"
+            onClick={() => setCash(!cash)}
+          >
             <input
               type="radio"
               name="payment_type"
@@ -48,32 +52,35 @@ function PaymentDetails({ register }) {
             </label>
           </div>
         </div>
+        {cash ? null : (
+          <>
+            <div className="space-y-3">
+              <label htmlFor="emoney-number" {...textStyles}>
+                e-Money Number
+              </label>
+              <input
+                {...inputStyles}
+                placeholder="238521993"
+                id="emoney-number"
+                required
+                {...register("emoney_number")}
+              />
+            </div>
 
-        <div className="space-y-3">
-          <label htmlFor="emoney-number" {...textStyles}>
-            e-Money Number
-          </label>
-          <input
-            {...inputStyles}
-            placeholder="238521993"
-            id="emoney-number"
-            required
-            {...register("emoney_number")}
-          />
-        </div>
-
-        <div className="space-y-3">
-          <label htmlFor="emoney-pin" {...textStyles}>
-            e-Money PIN
-          </label>
-          <input
-            {...inputStyles}
-            placeholder="6891"
-            id="emoney-pin"
-            required
-            {...register("emoney_pin")}
-          />
-        </div>
+            <div className="space-y-3">
+              <label htmlFor="emoney-pin" {...textStyles}>
+                e-Money PIN
+              </label>
+              <input
+                {...inputStyles}
+                placeholder="6891"
+                id="emoney-pin"
+                required
+                {...register("emoney_pin")}
+              />
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
