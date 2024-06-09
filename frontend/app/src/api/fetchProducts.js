3e5336d37c42599e1f508ca676aa382de04c9326
Jio Bautista <98562544+JioBautista@users.noreply.github.com>
2024-06-09 11:38:21 -0400

    Update fetchProducts.js

--- frontend/app/src/api/fetchProducts.js
+++ frontend/app/src/api/fetchProducts.js
@@ -1,6 +1,6 @@
 export async function fetchProducts() {
   try {
-    const response = await fetch("/src/data/data.json");
+    const response = await fetch("/src/data/data.js");
     return response.json();
   } catch (error) {
     console.log(error);
