659c5d3348eb2433dd6e400583756c1c9c51dd7a
Jio Bautista <98562544+JioBautista@users.noreply.github.com>
2025-03-30 18:37:43 -0400

    Updates
    Updates

--- backend/web-server/app.js
+++ backend/web-server/app.js
@@ -1,5 +1,5 @@
-import express from "express";
-import { PrismaClient } from "@prisma/client";
+const express = require("express");
+const { PrismaClient } = require("@prisma/client");
 
 const prisma = new PrismaClient();
 const app = express();
