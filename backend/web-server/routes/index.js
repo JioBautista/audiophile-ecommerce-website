19d52ee44f49b2c3c2e09c5e0e7bde4e510a28e2
Jio Bautista <98562544+JioBautista@users.noreply.github.com>
2025-03-31 20:06:59 -0400

    Updates

--- backend/web-server/routes/index.js
+++ backend/web-server/routes/index.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const router = express.Router();
-const { PrismaClient } = require("../generated/prisma");
-const prisma = new PrismaClient();
+const cors = require("cors");
+const { Pool } = require("pg");
+require("dotenv").config();
 
-router.get("/", async (request, response) => {
-  const test_data = await prisma.playing_with_neon.findMany();
-  response.json(test_data);
+router.get("/", cors(), async (request, response) => {
+  const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+  const client = await pool.connect();
+  const result = await client.query(
+    `SELECT jsonb_agg(properties) AS data FROM products`
+  );
+  client.release();
+  console.log(result);
+  response.json(result.rows[0].data);
 });
 
 module.exports = router;
